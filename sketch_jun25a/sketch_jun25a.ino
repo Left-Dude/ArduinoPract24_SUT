9c655c6a2daa333ba52ee1f575cd6c96df3d962a
1NoMoney1 <vovalzg89@gamil.com>
2024-07-01 11:35:54 +0300

    add IK

--- sketch_jun25a/sketch_jun25a.ino
+++ sketch_jun25a/sketch_jun25a.ino
@@ -1,10 +1,8 @@
-#define popin 0
-
-
 #include "DHT.h"
 
 // #include "Bonezegei_DHT11.h"
 #define DHTPIN 2 // номер пина, к которому подсоединен датчик
+#define popin 0
 
 // Раскомментируйте в соответствии с используемым датчиком
 
@@ -21,25 +19,25 @@ void datMQ3() {
    // Detecting range: 20ppm-2000ppm carbon monoxide 
    // air quality-cases: < 200 perfect, 200 - 800 normal, > 800 - 1800 high, > 1800 abnormal 
   if (MQ3sensorValue <= 200)  
-   { 
-      Serial.println("Air-Quality: CO perfect"); 
-   } 
-   else if ((MQ3sensorValue > 200) || (MQ3sensorValue <= 800)) // || = or 
-   { 
-      Serial.println("Air-Quality: CO normal"); 
-   } 
-   else if ((MQ3sensorValue > 800) || (MQ3sensorValue <= 1800)) 
-   { 
-      Serial.println("Air-Quality: CO high"); 
-   } 
-   else if (MQ3sensorValue > 1800)  
-   { 
-      Serial.println("Air-Quality: ALARM CO very high");  
-   } 
-   else 
-   { 
-      Serial.println("MQ-3 - cant read any value - check the sensor!"); 
-   }  
+  { 
+    Serial.println("Air-Quality: CO perfect"); 
+  } 
+  else if ((MQ3sensorValue > 200) || (MQ3sensorValue <= 800)) // || = or 
+  { 
+    Serial.println("Air-Quality: CO normal"); 
+  } 
+  else if ((MQ3sensorValue > 800) || (MQ3sensorValue <= 1800)) 
+  { 
+    Serial.println("Air-Quality: CO high"); 
+  } 
+  else if (MQ3sensorValue > 1800)  
+  { 
+    Serial.println("Air-Quality: ALARM CO very high");  
+  } 
+  else 
+  { 
+    Serial.println("MQ-3 - cant read any value - check the sensor!"); 
+  }  
 }
 
 
@@ -61,12 +59,16 @@ void datDHT() {
   Serial.print("Влажность: "); Serial.print(h); Serial.print(" %\t"); Serial.print("Температура: "); Serial.print(t); Serial.print(" *C\n");
 }
 
+void datIKD(){
+  int v;
+  v = digitalRead( DHTPIN );
+  Serial.println(v); 
+}
+
 void setup() {
 
 analogWrite(popin, HIGH);
-
 Serial.begin(9600);
-
 dht.begin();
 
 }
@@ -74,8 +76,8 @@ dht.begin();
 void loop() {
 
 // Задержка 2 секунды между измерениями
-  datMQ3();
-
+  //datMQ3();
+  datIKD();
 
   delay(500);
 }
