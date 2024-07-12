00f504df4d73b4a575f03cdd54f6ae9d866fe4e0
CODE_SLAVE <Samo.sos.mira@gmail.com>
2024-07-12 12:51:59 +0300

    refactoring & adding featurs

--- sketch_jul3a/sketch_jul3a.ino
+++ sketch_jul3a/sketch_jul3a.ino
@@ -28,11 +28,11 @@ void setup() {
 
   // DEBUGING
   //
-  // JsonDocument ddc;
-  // for (int i = 0; i < 13; i++) {
-  //   ddc["calibrationVoltages"][i] = random(0,10);
-  //   ddc["calibrationConcentrations"][i] = random(0,10);
-  // }
+  JsonDocument ddc;
+  for (int i = 0; i < 13; i++) {
+    ddc["calibrationVoltages"][i] = random(0,10);
+    ddc["calibrationConcentrations"][i] = random(0,10);
+  }
 
   // Заполнение градуировочной характеристики
   generateCalibrationCurve();
@@ -45,8 +45,7 @@ void setup() {
   Serial.println(unit);
 
   // Коллибровка. Получение коэффициентов
-  calcUnits(calibrationVoltages, calibrationConcentrations);
-  //calcUnits(ddc);
+  calcUnits(ddc);
 }
 
 void loop() {
@@ -54,12 +53,18 @@ void loop() {
 
   serializeJson(doc, Serial);
   Serial.println((char)0x0A);
+  
   if (Serial1.available()) {
-
-    int inByte = Serial1.read();
-
-    Serial.write(inByte);
-
+    JsonDocument inJson;
+    String jsonString = Serial1.readStringUntil('\n');
+    DeserializationError error = deserializeJson(inJson, jsonString);
+      
+    if (error) {
+      Serial1.print(F("deserializeJson() failed: "));
+      Serial1.println(error.f_str());
+      return;
+    }
+    calcUnits(inJson);
   }
   delay(10000);  // Увеличен до 2000 мс для удобства чтения данных
 }
@@ -69,16 +74,30 @@ float polynomialCalibration(float x, float a0, float a1, float a2) {
 }
 
 void analogExchange() {
+  float lastConcentration1 = 0.0; // Переменная для хранения предыдущего значения концентрации
+  float lastConcentration2 = 0.0; // Переменная для хранения предыдущего значения концентрации
+
   int sensorValue = analogRead(ANALOGPIN);
   doc["sensorValue"] = sensorValue;
 
   float voltage = sensorValue * (5.0 / 1023.0);
   doc["voltage"] = voltage;
-
   float concentration1 = polynomialCalibration(voltage, a0, a1, a2);
   doc["concentration1"] = concentration1;
 
   float concentration2 = polynomialCalibration(voltage, a0, a1, a2);
+
+  // Вычисление погрешности измерения
+  float error1 = concentration1 - lastConcentration1;
+  float error2 = concentration2 - lastConcentration2;
+
+  // Сохранение погрешности в JSON
+  doc["error1"] = error1;
+  doc["error2"] = error2;
+
+  // Обновление предыдущих значений концентрации
+  lastConcentration1 = concentration1;
+  lastConcentration2 = concentration2;
 
   Serial.print("Напряжение: ");
   Serial.print(voltage);
@@ -115,10 +134,10 @@ void calcUnits(JsonDocument dds) {
   //   Voltages[i] = dds["calibrationVoltages"][i];
   //   Concentrations[i] = dds["calibrationConcentrations"][i];
   // }
-  int sz = dds["calibrationConcentrations"].size()-1;
-  int random_value_1 = random(0,sz);
-  int random_value_2 = random(0,sz);
-  int random_value_3 = random(0,sz); 
+  int size = dds["calibrationConcentrations"].size();
+  int random_value_1 = random(0,size);
+  int random_value_2 = random(0,size);
+  int random_value_3 = random(0,size); 
 
   float x1 = dds["calibrationVoltages"][random_value_1];
   float y1 = dds["calibrationConcentrations"][random_value_1];
@@ -126,66 +145,6 @@ void calcUnits(JsonDocument dds) {
   float y2 = dds["calibrationConcentrations"][random_value_2];
   float x3 = dds["calibrationVoltages"][random_value_3];
   float y3 = dds["calibrationConcentrations"][random_value_3];
-
-  // Вычисление коэффициентов A, B и C для квадратичной функции y = Ax^2 + Bx + C
-  float matrix[3][4] = {
-    {x1 * x1, x1, 1, y1},
-    {x2 * x2, x2, 1, y2},
-    {x3 * x3, x3, 1, y3}
-  };
-
-  // Прямой ход метода Гаусса
-  for (int i = 0; i < 3; i++) {
-    // Нормализация строки
-    float factor = matrix[i][i];
-    for (int j = 0; j < 4; j++) {
-      matrix[i][j] /= factor;
-    }
-
-    // Обнуление столбца
-    for (int k = i + 1; k < 3; k++) {
-      factor = matrix[k][i];
-      for (int j = 0; j < 4; j++) {
-        matrix[k][j] -= factor * matrix[i][j];
-      }
-    }
-  }
-
-  // Обратный ход метода Гаусса
-  for (int i = 2; i >= 0; i--) {
-    for (int k = i - 1; k >= 0; k--) {
-      float factor = matrix[k][i];
-      for (int j = 0; j < 4; j++) {
-        matrix[k][j] -= factor * matrix[i][j];
-      }
-    }
-  }
-
-  // Решение системы
-  float A = matrix[0][3];
-  float B = matrix[1][3];
-  float C = matrix[2][3];
-
-  Serial.println("Полученные коэффициенты:");
-  Serial.print("A = ");
-  Serial.println(A);
-  Serial.print("B = ");
-  Serial.println(B);
-  Serial.print("C = ");
-  Serial.println(C);
-}
-
-void calcUnits(float *_calibrationVoltages, float *_calibrationConcentrations) {
-  int random_value_1 = random(0,10);
-  int random_value_2 = random(0,10);
-  int random_value_3 = random(0,10); 
-
-  float x1 = _calibrationVoltages[random_value_1];
-  float y1 = _calibrationConcentrations[random_value_1];
-  float x2 = _calibrationVoltages[random_value_2];
-  float y2 = _calibrationConcentrations[random_value_2];
-  float x3 = _calibrationVoltages[random_value_3];
-  float y3 = _calibrationConcentrations[random_value_3];
 
   // Вычисление коэффициентов A, B и C для квадратичной функции y = Ax^2 + Bx + C
   float matrix[3][4] = {
