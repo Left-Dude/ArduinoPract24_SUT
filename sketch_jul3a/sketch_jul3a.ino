ccc76d09216a6bdd2ae6023207256deb3d5fef6d
1NoMoney1 <vovalzg89@gamil.com>
2024-07-16 15:23:35 +0300

    finely!!

--- sketch_jul3a/sketch_jul3a.ino
+++ sketch_jul3a/sketch_jul3a.ino
@@ -1,124 +1,107 @@
 #include "ArduinoJson.h"
 
-#define ANALOGPIN A0  // Аналоговый пин для датчика MQ-135
-#define DHTPIN 2 // номер пина, к которому подсоединен датчик
+#define ANALOGPIN A0  // Аналоговый пин для датчика
 
-float a0, a1, a2;
-float A, B, C;
+float a0, a1, a2; // Коэффициенты квадратичной калибровочной кривой
+float lastConcentration = 0.0; // Переменная для хранения предыдущего значения концентрации
 
-String sensorType;
-String unit;
+String sensorType; // Тип датчика
+String unit; // Единицы измерения
 
-JsonDocument doc;
+
+  //int sensorValue;
+  //float voltage;
+  //float concentration;
+  //float inaccuracy;
+
+JsonDocument doc; // JSON-документ для хранения данных
 
 // Градуировочная характеристика
-const int numPoints = 10;
-float calibrationVoltages[numPoints];
-float calibrationConcentrations[numPoints];
+const int numPoints = 10; // Количество точек градуировочной кривой
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  pinMode(ANALOGPIN, INPUT);
+  Serial.begin(9600); // Инициализация последовательного порта для монитора
+  pinMode(ANALOGPIN, INPUT); // Установка пина ANALOGPIN на вход
 
-  doc["sensor"] = sensorType = getInput("Введите тип датчика:");
-  doc["unit"] = unit = getInput("Выберите тип физической величины:");
+  //Получение и сохранение пользовательского ввода для типа датчика и единиц измерения
+  //doc["sensor"] = sensorType = getInput("Введите тип датчика: ");
+  //doc["unit"] = unit = getInput("Выберите тип физической величины: ");
+  
+  //Serial.println(sensorType);
+  //Serial.println(unit);
 
-  EnterPolinom();
+  // Ожидание ввода параметров от внешнего устройства
+  waitForParameters();
 
-  // DEBUGING
-  //
-  JsonDocument ddc;
-  for (int i = 0; i < 13; i++) {
-    ddc["calibrationVoltages"][i] = random(0,10);
-    ddc["calibrationConcentrations"][i] = random(0,10);
-  }
-
-  // Заполнение градуировочной характеристики
+  // Заполнение и вывод градуировочной кривой
   generateCalibrationCurve();
 
-
-  Serial.println("Настройка завершена.");
-  Serial.print("Тип датчика: ");
-  Serial.println(sensorType);
-  Serial.print("Единицы измерения: ");
-  Serial.println(unit);
-
-  // Коллибровка. Получение коэффициентов
-  calcUnits(ddc);
+  //Вывод информации о настройках
+  // Serial.println("");
+  // Serial.println("Настройка завершена.");
+  // Serial.print("Тип датчика: ");
+  // Serial.println(sensorType);
+  // Serial.print("Единицы измерения: ");
+  // Serial.println(unit);
+  // Serial.print("Коэффициент a0: ");
+  // Serial.println(a0);
+  // Serial.print("Коэффициент a1: ");
+  // Serial.println(a1);
+  // Serial.print("Коэффициент a2: ");
+  // Serial.println(a2);
 }
 
 void loop() {
-  analogExchange();
-
-  serializeJson(doc, Serial);
-  Serial.println((char)0x0A);
+  //serializeJson(doc, Serial); // Отправка JSON-документа на монитор последовательного порта
+  //Serial.println((char)0x0A); // Добавление символа новой строки (LF, ASCII 10)
+  waitForGradcients();
   
-  if (Serial1.available()) {
-    JsonDocument inJson;
-    String jsonString = Serial1.readStringUntil('\n');
-    DeserializationError error = deserializeJson(inJson, jsonString);
-      
-    if (error) {
-      Serial1.print(F("deserializeJson() failed: "));
-      Serial1.println(error.f_str());
-      return;
-    }
-    calcUnits(inJson);
-  }
-  delay(10000);  // Увеличен до 2000 мс для удобства чтения данных
+  // // Вывод данных на монитор последовательного порта
+  // Serial.print("Напряжение: ");
+  // Serial.print(voltage);
+  // Serial.print(" V\t");
+  // Serial.print("Концентрация: ");
+  // Serial.print(concentration);
+  // Serial.print(" ");
+  // Serial.println(unit);
+  delay(500); // Задержка для удобства чтения данных
 }
 
+// Функция калибровки: вычисление значения концентрации по калибровочной кривой
 float polynomialCalibration(float x, float a0, float a1, float a2) {
   return a0 + a1 * x + a2 * x * x;
 }
 
+// Функция обмена данными с аналоговым датчиком
 void analogExchange() {
-  float lastConcentration1 = 0.0; // Переменная для хранения предыдущего значения концентрации
-  float lastConcentration2 = 0.0; // Переменная для хранения предыдущего значения концентрации
+  int sensorValue = analogRead(ANALOGPIN); // Чтение значения с аналогового пина
+  doc["sensorValue"] = sensorValue; // Сохранение значения в JSON-документе
 
-  int sensorValue = analogRead(ANALOGPIN);
-  doc["sensorValue"] = sensorValue;
+  float voltage = sensorValue * (5.0 / 1023.0); // Преобразование значения в напряжение
+  doc["voltage"] = voltage; // Сохранение напряжения в JSON-документе
 
-  float voltage = sensorValue * (5.0 / 1023.0);
-  doc["voltage"] = voltage;
-  float concentration1 = polynomialCalibration(voltage, a0, a1, a2);
-  doc["concentration1"] = concentration1;
+  float concentration = polynomialCalibration(voltage, a0, a1, a2); // Вычисление концентрации
+  doc["concentration"] = concentration; // Сохранение концентрации в JSON-документе
 
-  float concentration2 = polynomialCalibration(voltage, a0, a1, a2);
+  // Вычисление погрешности измерения и сохранение в JSON
+  float inaccuracy = abs(concentration - lastConcentration);
+  doc["inaccuracy"] = inaccuracy;
 
-  // Вычисление погрешности измерения
-  float error1 = concentration1 - lastConcentration1;
-  float error2 = concentration2 - lastConcentration2;
+  // Обновление предыдущего значения концентрации
+  lastConcentration = concentration;
 
-  // Сохранение погрешности в JSON
-  doc["error1"] = error1;
-  doc["error2"] = error2;
-
-  // Обновление предыдущих значений концентрации
-  lastConcentration1 = concentration1;
-  lastConcentration2 = concentration2;
-
-  Serial.print("Напряжение: ");
-  Serial.print(voltage);
-  Serial.print(" V\t");
-  Serial.print("Концентрация: ");
-  Serial.print(concentration1);
-  Serial.print(" ");
-  Serial.println(unit);
-
-  Serial.print("Новая Концентрация: ");
-  Serial.print(concentration2);
-  Serial.print(" ");
-  Serial.println(unit);
 }
 
-void generateCalibrationCurve(){
+// Генерация градуировочной кривой
+void generateCalibrationCurve() {
   Serial.println("Градуировочная характеристика:");
 
   for (int i = 0; i < numPoints; i++) {
-    doc["calibrationVoltages"][i] = calibrationVoltages[i] = i * (5.0 / (numPoints - 1)); // Разделение диапазона 0-5V на равные части
-    doc["calibrationConcentrations"][i] = calibrationConcentrations[i] = polynomialCalibration(doc["calibrationVoltages"][i], a0, a1, a2);
+    // Заполнение массивов напряжений и концентраций в JSON-документе
+    doc["calibrationVoltages"][i] = i * (5.0 / (numPoints - 1));
+    doc["calibrationConcentrations"][i] = polynomialCalibration(doc["calibrationVoltages"][i], a0, a1, a2);
+
+    // Вывод данных на монитор последовательного порта
     Serial.print("Напряжение: ");
     Serial.print((float)doc["calibrationVoltages"][i]);
     Serial.print(" V -> Концентрация: ");
@@ -128,40 +111,37 @@ void generateCalibrationCurve(){
   }
 }
 
+// Расчет коэффициентов квадратичной калибровочной кривой по полученному JSON-документу
 void calcUnits(JsonDocument dds) {
-  // float Voltages [numPoints], Concentrations [numPoints];
-  // for (int i = 0; i < numPoints; i++) {
-  //   Voltages[i] = dds["calibrationVoltages"][i];
-  //   Concentrations[i] = dds["calibrationConcentrations"][i];
-  // }
-  int size = dds["calibrationConcentrations"].size();
-  int random_value_1 = random(0,size);
-  int random_value_2 = random(0,size);
-  int random_value_3 = random(0,size); 
+  int size = dds["calibrationConcentrations"].size(); // Размер массива концентраций
+  int random_value_1 = random(0, size); // Генерация случайного индекса 1
+  int random_value_2 = random(0, size); // Генерация случайного индекса 2
+  int random_value_3 = random(0, size); // Генерация случайного индекса 3
 
-  float x1 = dds["calibrationVoltages"][random_value_1];
-  float y1 = dds["calibrationConcentrations"][random_value_1];
-  float x2 = dds["calibrationVoltages"][random_value_2];
-  float y2 = dds["calibrationConcentrations"][random_value_2];
-  float x3 = dds["calibrationVoltages"][random_value_3];
-  float y3 = dds["calibrationConcentrations"][random_value_3];
+  // Извлечение напряжений и концентраций по случайным индексам
+  double x1 = dds["calibrationVoltages"][random_value_1];
+  double y1 = dds["calibrationConcentrations"][random_value_1];
+  double x2 = dds["calibrationVoltages"][random_value_2];
+  double y2 = dds["calibrationConcentrations"][random_value_2];
+  double x3 = dds["calibrationVoltages"][random_value_3];
+  double y3 = dds["calibrationConcentrations"][random_value_3];
 
-  // Вычисление коэффициентов A, B и C для квадратичной функции y = Ax^2 + Bx + C
-  float matrix[3][4] = {
+  // Вычисление коэффициентов квадратичной калибровочной кривой методом Гаусса
+  double matrix[3][4] = {
     {x1 * x1, x1, 1, y1},
     {x2 * x2, x2, 1, y2},
     {x3 * x3, x3, 1, y3}
   };
 
-  // Прямой ход метода Гаусса
+  // Прямой ход метода Гаусса для решения системы уравнений
   for (int i = 0; i < 3; i++) {
-    // Нормализация строки
-    float factor = matrix[i][i];
+    // Нормализация текущей строки
+    double factor = matrix[i][i];
     for (int j = 0; j < 4; j++) {
       matrix[i][j] /= factor;
     }
 
-    // Обнуление столбца
+    // Обнуление столбца под текущим элементом
     for (int k = i + 1; k < 3; k++) {
       factor = matrix[k][i];
       for (int j = 0; j < 4; j++) {
@@ -170,42 +150,104 @@ void calcUnits(JsonDocument dds) {
     }
   }
 
-  // Обратный ход метода Гаусса
+  // Обратный ход метода Гаусса для решения системы уравнений
   for (int i = 2; i >= 0; i--) {
     for (int k = i - 1; k >= 0; k--) {
-      float factor = matrix[k][i];
+      double factor = matrix[k][i];
       for (int j = 0; j < 4; j++) {
         matrix[k][j] -= factor * matrix[i][j];
       }
     }
   }
+  // Получение решения системы и установка коэффициентов квадратичной калибровочной кривой
+  a2 = matrix[0][3];
+  a1 = matrix[1][3];
+  a0 = matrix[2][3];
 
-  // Решение системы
-  float A = matrix[0][3];
-  float B = matrix[1][3];
-  float C = matrix[2][3];
-
+  // Вывод коэффициентов на монитор последовательного порта
   Serial.println("Полученные коэффициенты:");
   Serial.print("A = ");
-  Serial.println(A);
+  Serial.println(a2);
   Serial.print("B = ");
-  Serial.println(B);
+  Serial.println(a1);
   Serial.print("C = ");
-  Serial.println(C);
+  Serial.println(a0);
 }
 
-void EnterPolinom() {
-  a0 = getFloatInput("Введите коэффициент a0: ");
-  a1 = getFloatInput("Введите коэффициент a1: ");
-  a2 = getFloatInput("Введите коэффициент a2: ");
+// Ожидание и получение коэффициентов от внешнего устройства через последовательный порт
+void waitForParameters() {
+  StaticJsonDocument<256> docJson; // JSON-документ для коэффициентов
+  Serial.println("Ожидание коэффициентов от внешнего устройства...");
+  while (true) {
+    if (Serial.available()) {
+      String coefString = Serial.readStringUntil('\n');
+      DeserializationError error = deserializeJson(docJson, coefString);
+      if (error) {
+        Serial.print(F("Ошибка десериализации JSON: "));
+        Serial.println(error.f_str());
+        continue; // Пропуск текущей итерации и ожидание следующего ввода
+      }
+
+      // Проверка наличия всех коэффициентов в JSON-документе
+      if (docJson.containsKey("a0") && docJson.containsKey("a1") && docJson.containsKey("a2") && docJson.containsKey("sensorType") && docJson.containsKey("unit")) {
+        a0 = docJson["a0"];
+        a1 = docJson["a1"];
+        a2 = docJson["a2"];
+
+        //Получение и сохранение пользовательского ввода для типа датчика и единиц измерения
+        doc["sensor"] = docJson["sensor"];
+        doc["unit"] = docJson["unit"];
+             
+        Serial.println("Коэффициенты получены:");
+        Serial.print("a0 = ");
+        Serial.println(a0);
+        Serial.print("a1 = ");
+        Serial.println(a1);
+        Serial.print("a2 = ");
+        Serial.println(a2);
+        break; // Выход из цикла после получения коэффициентов
+      } else {
+        Serial.println("JSON-документ не содержит все необходимые коэффициенты. Повторите ввод.");
+      }
+    }
+  }
 }
 
+void waitForGradcients() {
+  StaticJsonDocument<256> inJson; // Входящий JSON-документ
+  while(true){
+    if (Serial.available()) {
+      String inputString = Serial.readStringUntil('\n'); // Чтение строки из внешнего последовательного порта
+      DeserializationError error = deserializeJson(inJson, inputString);
+      if (inJson.containsKey("calibrationVoltages") & inJson.containsKey("calibrationConcentrations")) {   
+        if (error) {
+          // Обработка ошибки десериализации
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.f_str());
+          continue;
+        }
+
+        // Выполнение калибровки и расчета коэффициентов по полученному JSON-документу
+        calcUnits(inJson);
+        break;
+      } else if (inputString == "getJson") {
+        // Если получена команда "getJson", отправляем текущий JSON-документ через внешний порт
+        serializeJson(doc, Serial);
+        Serial.println((char)0x0A);
+      }
+    } 
+    analogExchange(); // Запуск обмена данными с аналоговым датчиком
+  }
+}
+
+// Функция получения строки ввода от пользователя через последовательный порт
 String getInput(String prompt) {
   Serial.println(prompt);
   while (!Serial.available()) {}
   return Serial.readStringUntil('\n');
 }
 
+// Функция получения числового ввода от пользователя через последовательный порт
 float getFloatInput(String prompt) {
   Serial.println(prompt);
   while (!Serial.available()) {}
